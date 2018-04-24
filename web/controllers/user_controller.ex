b4e06c338f27c4e6c27e1d05fa4d68e01fe938b0
Rafael Roman <31549567+RomanNumerals@users.noreply.github.com>
2018-04-24 19:30:03 -0400

    first big code push from chapters 3-5

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,1 +1,29 @@
+defmodule Dank.UserController do
+  use Dank.Web, :controller
 
+  def index(conn, _params) do
+    users = Repo.all(Dank.User)
+    render(conn, "index.html", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Dank.User, id)
+    render(conn, "show.html", user: user)
+  end
+
+  alias Dank.User
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
+  end
+end
