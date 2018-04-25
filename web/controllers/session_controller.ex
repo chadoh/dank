762814243542c709fc1f54ffc4e3d9e131052f10
Rafael Roman <31549567+RomanNumerals@users.noreply.github.com>
2018-04-24 20:17:55 -0400

    other half of the 3 chapters

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,1 +1,22 @@
+defmodule Dank.SessionController do
+  use Dank.Web, :controller
 
+  def new(conn, _) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"username" => user, "password" => pass}}) do
+    case Dank.Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+
+  def delet(conn, _) do
+    conn
+    |> Dank.Auth.logut()
+    |> redirect(to: page_path(conn, :index))
+  end
+end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,9 +1,12 @@
 defmodule Dank.UserController do
   use Dank.Web, :controller
 
+  plug(:authenticate when action in [:index, :show])
+
   def index(conn, _params) do
-    users = Repo.all(Dank.User)
-    render(conn, "index.html", users: users)
+        users = Repo.all(Dank.User)
+        render(conn, "index.html", users: users)
+    end
   end
 
   def show(conn, %{"id" => id}) do
@@ -19,11 +22,28 @@ defmodule Dank.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
-    {:ok, user} = Repo.insert(changeset)
+    changeset = User.registration_changeset(%User{}, user_params)
 
-    conn
-    |> put_flash(:info, "#{user.name} created!")
-    |> redirect(to: user_path(conn, :index))
+    case(Repo.insert(changeset)) do
+      {:ok, user} ->
+        conn
+        |> Dank.Auth.login(user)
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
