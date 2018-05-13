95b51833ef5690d6862130ca0827094ba79bee6d
Chad Ostrowski <hi@chadoh.com>
2018-05-13 12:02:34 -0400

    Add seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,3 +10,14 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 # Create 10 dispensaries in this part
+
+alias Dank.Dispensary
+
+[
+  %Dispensary{ name: "Nice Nugs", location: "24 E King Street", description: "Find all the best nugs here" },
+  %Dispensary{ name: "Best Buds", location: "52 N Lime Street", description: "If you need a bud, we got u" },
+]
+|> Enum.each(fn dispensary ->
+  Dank.Repo.get_by(Dispensary, name: dispensary.name) ||
+    Dank.Repo.insert!(dispensary)
+end)
